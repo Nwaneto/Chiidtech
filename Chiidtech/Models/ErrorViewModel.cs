8d9639104f65f168c789df6563d8aca788d62fe8
Chidi Nwaneto <Chidi.Nwaneto@fcmb.com>
2022-08-21 00:18:11 +0100

    Add project files.

---
+++ Chiidtech/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chiidtech.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
